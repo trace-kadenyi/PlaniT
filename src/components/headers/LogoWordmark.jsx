0b67f9550a8a8de76fc597c11391c7ac466efaa0
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-16 19:53:01 +0300

    Add logowordmark

---
+++ src/components/headers/LogoWordmark.jsx
@@ -0,0 +1,34 @@
+export default function LogoWordmark() {
+  return (
+    <div>
+      <svg
+        viewBox="0 0 300 100"
+        width="120"
+        height="40"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <text
+          x="10"
+          y="65"
+          fontFamily="'Montserrat', sans-serif"
+          fontWeight="700"
+          fontSize="64"
+          fill="#FFFFFF"
+        >
+          Plani
+        </text>
+
+        <text
+          x="185"
+          y="65"
+          fontFamily="'Montserrat', sans-serif"
+          fontWeight="700"
+          fontSize="64"
+          fill="#F59E0B"
+        >
+          T
+        </text>
+      </svg>
+    </div>
+  );
+}
