a84632d76cbb79dca1a7dc4f66c94da5230d4adc
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-16 18:05:44 +0300

    Add logoicon component

---
+++ src/components/headers/Logoicon.jsx
@@ -0,0 +1,19 @@
+export default function LogoIcon() {
+  return (
+    <div className="w-10 h-10">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="40"
+        height="40"
+        viewBox="0 0 100 100"
+        fill="none"
+      >
+        <circle cx="50" cy="50" r="10" fill="#9B2C62" />
+        <circle cx="20" cy="50" r="5" fill="#F59E0B" />
+        <circle cx="80" cy="50" r="5" fill="#F59E0B" />
+        <circle cx="50" cy="20" r="5" fill="#F59E0B" />
+        <circle cx="50" cy="80" r="5" fill="#F59E0B" />
+      </svg>
+    </div>
+  );
+}
