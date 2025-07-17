6e946f6f6835d38697c88cab19c7e0fb3c2f817d
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 19:50:38 +0300

    Store backend url

---
+++ src/services/api.jsx
@@ -0,0 +1,9 @@
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+export default api;
