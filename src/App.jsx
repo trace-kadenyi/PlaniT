e45cdf6b291d568cb6bc833964de2ff46c01de6f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 15:37:31 +0300

    Add temp footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-      <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
