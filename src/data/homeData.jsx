ebdbacf293ed815094e785f7e8d05a4990d6ff7b
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 15:05:12 +0300

    Add icon

--- src/data/homeData.jsx
+++ src/data/homeData.jsx
@@ -1,3 +1,10 @@
+import {
+  CalendarClockIcon,
+  ClipboardList,
+  Users,
+  BarChart4,
+} from "lucide-react";
+
 // features
 export const features = [
   {
@@ -29,18 +36,22 @@ export const features = [
 // process
 export const steps = [
   {
+    Icon: CalendarClockIcon,
     title: "1. Set Up Your Event",
     text: "Start by creating your event and setting a budget.",
   },
   {
+    Icon: ClipboardList,
     title: "2. Plan and Assign Tasks",
     text: "Break down the event into manageable tasks and assign them.",
   },
   {
+    Icon: Users,
     title: "3. Manage Vendors & Resources",
     text: "Store vendor info and track bookings in one place.",
   },
   {
+    Icon: BarChart4,
     title: "4. Track Progress & Budget",
     text: "Stay on top of your tasks, finances, and calendar deadlines.",
   },
