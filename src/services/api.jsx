1974c8e261569e30c0e0558d632a110b58cd181f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 20:11:19 +0300

    update base url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,9 +1,7 @@
-const baseURL = import.meta.env.VITE_API_BASE_URL;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "http://localhost:4000", // backend base URL
 });
 
 export default api;
