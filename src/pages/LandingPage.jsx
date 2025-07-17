f6c83547bf69bb6e297151026fadd2a234bbf5f1
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 14:52:21 +0300

    use easeout func on the footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,6 +10,7 @@ import {
   fadeUp,
   StepCard,
   delayedFadeUp,
+  EaseOutFunc,
 } from "../components/accessories/FramerMotion";
 
 export default function LandingPage() {
@@ -116,18 +117,20 @@ export default function LandingPage() {
       </ScrollFadeFunc>
 
       {/* Call to Action Section */}
-      <section className="bg-[#9B2C62] text-white py-16 px-6 text-center">
-        <div className="max-w-3xl mx-auto">
-          <h3 className="text-3xl font-bold mb-4">Ready to plan smarter?</h3>
-          <p className="mb-6 text-lg">
-            Join event pros using PlaniT to bring structure and clarity to their
-            events. It’s your turn to plan like a pro.
-          </p>
-          <button className="bg-[#F59E0B] text-black font-semibold px-8 py-3 rounded hover:bg-[#d97706] transition">
-            Sign Up Free
-          </button>
-        </div>
-      </section>
+      <EaseOutFunc>
+        <section className="bg-[#9B2C62] text-white py-16 px-6 text-center">
+          <div className="max-w-3xl mx-auto">
+            <h3 className="text-3xl font-bold mb-4">Ready to plan smarter?</h3>
+            <p className="mb-6 text-lg">
+              Join event pros using PlaniT to bring structure and clarity to
+              their events. It’s your turn to plan like a pro.
+            </p>
+            <button className="bg-[#F59E0B] text-black font-semibold px-8 py-3 rounded hover:bg-[#d97706] transition">
+              Sign Up Free
+            </button>
+          </div>
+        </section>
+      </EaseOutFunc>
     </main>
   );
 }
