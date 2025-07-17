1aa834a7ef9f2a72b5b849a555336c0114fd89d1
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 20:11:29 +0300

    use test events

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import Footer from "./components/footer/Footer";
+import TestEvents from "./components/tests/TestEvents";
 
 function App() {
   return (
     <>
+     <div>
+      <TestEvents />
+    </div>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
