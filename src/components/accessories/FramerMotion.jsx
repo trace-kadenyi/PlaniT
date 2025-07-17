248f407581d86d1060d21b4d16fb167c61c27dfa
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 14:46:50 +0300

    Hover function with framer motion

--- src/components/accessories/FramerMotion.jsx
+++ src/components/accessories/FramerMotion.jsx
@@ -34,5 +34,18 @@ export const EaseOutFunc = ({ children }) => {
   );
 };
 
+// hover function
+export const HoverFunc = ({ children, className }) => {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.02 }}
+      transition={{ type: "spring", stiffness: 300 }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
 
 
