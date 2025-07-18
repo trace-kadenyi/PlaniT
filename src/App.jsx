1c046d4dfc9963ad910806681372617193f14138
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-18 14:01:53 +0300

    Add dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import Footer from "./components/footer/Footer";
+import Dashboard from "./components/dashboard/Dashboard"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
         <Footer />
       </Router>
