685c2aab71a2e2459b3ffce62076dab97d68a543
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-18 15:47:07 +0300

    Add route to edit event form

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import LandingPage from "./pages/LandingPage";
 import Footer from "./components/footer/Footer";
 import Dashboard from "./components/dashboard/Dashboard";
 import Event from "./components/dashboard/Event";
+import EditEventForm from "./components/taskManagerFolders/events/EditEventForm";
 
 function App() {
   return (
     <>
       <Router>
-        <PrimaryHeader/>
+        <PrimaryHeader />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/events/:id" element={<Event />} />
+          <Route path="/events/:id/edit" element={<EditEventForm />} />
         </Routes>
         <Footer />
       </Router>
