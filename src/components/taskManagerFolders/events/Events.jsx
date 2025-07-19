f21d6de9bb38fedcece23b5ccf908a40d32cbca1
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-19 18:36:13 +0300

    remove unused code

--- src/components/taskManagerFolders/events/Events.jsx
+++ src/components/taskManagerFolders/events/Events.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEvents, deleteEvent } from "../../../redux/eventsSlice";
 import { useNavigate } from "react-router-dom";
