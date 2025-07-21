2fa85bc233c46b9eeb80febf9c223f0fe4cc1621
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-21 13:44:42 +0300

    create event handler to delete events

---
+++ src/components/taskManagerFolders/utils/handlers/eventHandlers.jsx
@@ -0,0 +1,32 @@
+import toast from "react-hot-toast";
+
+import { toastWithProgress } from "../toastWithProgress";
+import DeleteConfirmationToast from "../deleteConfirmationToast";
+
+export const createEventDeleteHandler = (
+  dispatch,
+  id,
+  navigate,
+  deleteEvent
+) => {
+  return () => {
+    const duration = 10000;
+    toast(
+      (t) => (
+        <DeleteConfirmationToast
+          t={t}
+          duration={duration}
+          type="event"
+          onConfirm={() => {
+            dispatch(deleteEvent(id));
+            toast.dismiss(t.id);
+            toastWithProgress("Event deleted successfully");
+            navigate("/events");
+          }}
+          onCancel={() => toast.dismiss(t.id)}
+        />
+      ),
+      { duration, position: "top-center" }
+    );
+  };
+};
