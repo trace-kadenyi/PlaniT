793e62a612e7f18fade3fc85fca296b0cbaf946e
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-21 22:31:08 +0300

    improve form fields UI

--- src/components/taskManagerFolders/tasks/forms/TaskFormFields.jsx
+++ src/components/taskManagerFolders/tasks/forms/TaskFormFields.jsx
@@ -68,8 +68,8 @@ export default function TaskFormFields({
       </div>
 
       {/* Deadline */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
+      <div className="relative">
+        <label className="block text-sm font-semibold text-[#9B2C62] mb-1">
           Deadline
         </label>
         <input
@@ -79,6 +79,20 @@ export default function TaskFormFields({
           onChange={onFieldChange}
           className="mt-1 w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#9B2C62]"
         />
+        <div className="absolute right-3 top-12 transform -translate-y-1/2 pointer-events-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-[#9B2C62]"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
       </div>
 
       {/* Priority & Status */}
