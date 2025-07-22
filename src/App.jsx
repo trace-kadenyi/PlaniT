4f9d28776c819075702756bdb6d1a946e11f976f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-22 18:16:30 +0300

    test kanban board route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Events from "./pages/Events";
 import Event from "./pages/Event";
 import EditEventForm from "./components/taskManagerFolders/events/forms/EditEventForm";
 import CreateEventForm from "./components/taskManagerFolders/events/forms/CreateEventForm";
+import KanbanBoard from "./pages/KanbanBoard";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/events/:id" element={<Event />} />
           <Route path="/events/:id/edit" element={<EditEventForm />} />
           <Route path="/events/create" element={<CreateEventForm />} />
+<Route path="/board" element={<KanbanBoard />} />
         </Routes>
         <Footer />
       </Router>
