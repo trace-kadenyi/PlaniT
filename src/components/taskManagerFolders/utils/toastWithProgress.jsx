a6ed1d9f3cebc00e80bbf5934d67c308a79111fe
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-23 17:49:44 +0300

    Add task toast progress to handle dashboard updates

--- src/components/taskManagerFolders/utils/toastWithProgress.jsx
+++ src/components/taskManagerFolders/utils/toastWithProgress.jsx
@@ -1,7 +1,18 @@
 import toast from "react-hot-toast";
 import CustomToast from "../../ui/CustomToast";
 
+// toast with progress
 export function toastWithProgress(message, duration = 4000) {
+  toast.custom(
+    (t) => <CustomToast t={t} message={message} duration={duration} />,
+    {
+      duration,
+    }
+  );
+}
+
+// task toast progress
+export function taskToastProgress(message, duration = 8000) {
   toast.custom(
     (t) => <CustomToast t={t} message={message} duration={duration} />,
     {
