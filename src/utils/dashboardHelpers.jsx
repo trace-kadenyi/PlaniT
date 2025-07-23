1bda553c8e8c0a1b346c99501ebff61255a88c74
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-23 15:20:02 +0300

    refactor: move loadingstate to utils/dashboardHelpers

--- src/utils/dashboardHelpers.jsx
+++ src/utils/dashboardHelpers.jsx
@@ -115,3 +115,14 @@ export const handleDragEnd = async (
     console.error("Task update failed:", err);
   }
 };
+
+// loading state
+export const LoadingDashboard = () => {
+  return (
+    <div className="flex space-x-4 animate-pulse justify-center">
+      {[1, 2, 3, 4].map((i) => (
+        <div key={i} className="bg-gray-100 rounded-lg p-4 w-72 h-64"></div>
+      ))}
+    </div>
+  );
+};
