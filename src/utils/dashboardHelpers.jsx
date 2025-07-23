8537d79d4a45b3c2eabd5b9766472621defe7ccf
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-23 15:04:27 +0300

    refactor: move getColumnsFromTasks to utils

--- src/utils/dashboardHelpers.jsx
+++ src/utils/dashboardHelpers.jsx
@@ -12,3 +12,39 @@ export const mapTaskToCard = (task) => ({
   status: task.status,
 });
 
+// get columns from tasks func
+export const getColumnsFromTasks = (sortedTasks, mapTaskToCardFn) => {
+  const filteredTasks = {
+    todo: sortedTasks.filter((task) => task.status === "To Do"),
+    inProgress: sortedTasks.filter((task) => task.status === "In Progress"),
+    inReview: sortedTasks.filter((task) => task.status === "In Review"),
+    completed: sortedTasks.filter((task) => task.status === "Completed"),
+  };
+
+  return {
+    todo: {
+      id: "todo",
+      title: "To Do",
+      tasks: filteredTasks.todo.map(mapTaskToCardFn),
+      color: "#F59E0B",
+    },
+    inProgress: {
+      id: "inProgress",
+      title: "In Progress",
+      tasks: filteredTasks.inProgress.map(mapTaskToCardFn),
+      color: "#FF7E33",
+    },
+    inReview: {
+      id: "inReview",
+      title: "In Review",
+      tasks: filteredTasks.inReview.map(mapTaskToCardFn),
+      color: "#9B2C62",
+    },
+    completed: {
+      id: "completed",
+      title: "Completed",
+      tasks: filteredTasks.completed.map(mapTaskToCardFn),
+      color: "#4CAF50",
+    },
+  };
+};
