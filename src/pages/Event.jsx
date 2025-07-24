596bd9f3a9742d465d7cf30b5ea75ef768a7e92a
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-25 02:01:19 +0300

    extract tab btns into utils

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -20,6 +20,7 @@ import { createEventDeleteHandler } from "../components/taskManagerCollection/ut
 import { createTaskDeleteHandler } from "../components/taskManagerCollection/utils/handlers/taskHandlers";
 import TasksTab from "../components/taskManagerCollection/tasks/TasksTab";
 import BudgetTab from "../components/taskManagerCollection/events/BudgetTab";
+import TabsBtns from "../components/taskManagerCollection/utils/tabBtns";
 
 export default function Event() {
   const { id } = useParams();
@@ -142,30 +143,9 @@ export default function Event() {
         </div>
       </div>
 
-      {/* Tasks Section */}
-      {/* Tab buttons */}
-      <div className="flex border-b border-gray-200 mb-6 space-x-6">
-        <button
-          onClick={() => setActiveTab("tasks")}
-          className={`pb-2 border-b-2 transition-all ${
-            activeTab === "tasks"
-              ? "border-[#9B2C62] text-[#9B2C62] font-semibold"
-              : "border-transparent text-gray-500 hover:text-[#9B2C62]"
-          }`}
-        >
-          Tasks
-        </button>
-        <button
-          onClick={() => setActiveTab("budget")}
-          className={`pb-2 border-b-2 transition-all ${
-            activeTab === "budget"
-              ? "border-[#F59E0B] text-[#F59E0B] font-semibold"
-              : "border-transparent text-gray-500 hover:text-[#F59E0B]"
-          }`}
-        >
-          Budget
-        </button>
-      </div>
+      {/* Tabs Section */}
+      {/* btns */}
+      <TabsBtns activeTab={activeTab} setActiveTab={setActiveTab} />
 
       {/* tasks tab */}
       {activeTab === "tasks" && (
