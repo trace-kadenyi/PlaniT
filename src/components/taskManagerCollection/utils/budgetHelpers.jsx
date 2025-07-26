6c5622cde97310ddb2bb91ba85e6f4e26023f56f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-26 19:52:36 +0300

    func to get expenses by category

--- src/components/taskManagerCollection/utils/budgetHelpers.jsx
+++ src/components/taskManagerCollection/utils/budgetHelpers.jsx
@@ -1,13 +1,27 @@
 // get budget status
 export const getBudgetStatus = (budget, expenses) => {
-  const totalExpenses = expenses.reduce((sum, expense) => sum + expense.amount, 0);
+  const totalExpenses = expenses.reduce(
+    (sum, expense) => sum + expense.amount,
+    0
+  );
   const remainingBudget = budget.totalBudget - totalExpenses;
-  
+
   return {
     totalBudget: budget.totalBudget,
     totalExpenses,
     remainingBudget,
     percentageUsed: (totalExpenses / budget.totalBudget) * 100,
-    isOverBudget: remainingBudget < 0
+    isOverBudget: remainingBudget < 0,
   };
+};
+
+// get expenses by category
+export const getExpensesByCategory = (expenses) => {
+  return expenses.reduce((acc, expense) => {
+    if (!acc[expense.category]) {
+      acc[expense.category] = 0;
+    }
+    acc[expense.category] += expense.amount;
+    return acc;
+  }, {});
 };
