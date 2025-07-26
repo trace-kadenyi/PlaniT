cd787503308f69ca1e1d98afb94afa7556b302db
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-26 19:56:09 +0300

    add reducers

--- src/redux/expensesSlice.jsx
+++ src/redux/expensesSlice.jsx
@@ -49,3 +49,47 @@ export const deleteExpense = createAsyncThunk(
   }
 );
 
+const expensesSlice = createSlice({
+  name: "expenses",
+  initialState: {
+    items: [],
+    budgetStatus: null,
+    status: "idle",
+    error: null,
+    createStatus: "idle",
+    createError: null,
+    updateStatus: "idle",
+    updateError: null,
+    deleteStatus: "idle",
+    deleteError: null,
+  },
+  reducers: {
+    resetExpenseStatuses: (state) => {
+      state.createStatus = "idle";
+      state.createError = null;
+      state.updateStatus = "idle";
+      state.updateError = null;
+      state.deleteStatus = "idle";
+      state.deleteError = null;
+    },
+  },
+  extraReducers: (builder) => {
+    // Fetch expenses
+    builder
+      .addCase(fetchExpenses.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchExpenses.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload.expenses;
+        state.budgetStatus = action.payload.budgetStatus;
+      })
+      .addCase(fetchExpenses.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+
+ 
+  },
+});
+
