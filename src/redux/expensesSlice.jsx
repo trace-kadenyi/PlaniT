e1cb904522c79f5a485ee438eccbdc537540b606
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-26 19:55:01 +0300

    create expense thunks

---
+++ src/redux/expensesSlice.jsx
@@ -0,0 +1,51 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../app/api";
+
+// Async Thunks
+export const fetchExpenses = createAsyncThunk(
+  "expenses/fetchExpenses",
+  async (eventId) => {
+    const res = await api.get(`/api/expenses/event/${eventId}`);
+    return res.data;
+  }
+);
+
+// create expense
+export const createExpense = createAsyncThunk(
+  "expenses/createExpense",
+  async (newExpense, { rejectWithValue }) => {
+    try {
+      const res = await api.post("/api/expenses", newExpense);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+// update expense
+export const updateExpense = createAsyncThunk(
+  "expenses/updateExpense",
+  async ({ id, updatedExpense }, { rejectWithValue }) => {
+    try {
+      const res = await api.put(`/api/expenses/${id}`, updatedExpense);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+// delete expense
+export const deleteExpense = createAsyncThunk(
+  "expenses/deleteExpense",
+  async (id, { rejectWithValue }) => {
+    try {
+      await api.delete(`/api/expenses/${id}`);
+      return id;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
