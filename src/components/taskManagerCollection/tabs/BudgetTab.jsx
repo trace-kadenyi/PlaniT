9a807df261872a29c644cc9908e890bb3ef6fab5
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-27 19:05:33 +0300

    refactor: extract functionality to handle expense edit and delete buttons

--- src/components/taskManagerCollection/tabs/BudgetTab.jsx
+++ src/components/taskManagerCollection/tabs/BudgetTab.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect, useRef } from "react";
-import { Plus, XCircle, Pencil, Trash2 } from "lucide-react";
+import { useState, useRef } from "react";
+import { Plus, XCircle } from "lucide-react";
 
 import { getExpensesByCategory } from "../utils/budgetHelpers";
 import EditExpenseForm from "../expenses/forms/EditExpenseForm";
 import CreateExpenseForm from "../expenses/forms/CreateExpenseForm";
+import EditDeleteExpense from "../../shared/EditDeleteExpense";
 
 export default function BudgetTab({
   expenses,
@@ -12,7 +13,6 @@ export default function BudgetTab({
 }) {
   const [showCreateExpenseForm, setShowCreateExpenseForm] = useState(false);
   const [expenseToEdit, setExpenseToEdit] = useState(null);
-  const [scrollToForm, setScrollToForm] = useState(false);
   const [activeView, setActiveView] = useState("list");
 
   const formRef = useRef(null);
@@ -23,13 +23,6 @@ export default function BudgetTab({
     : expenses?.items || [];
   const expensesByCategory = getExpensesByCategory(expensesArray);
   const isLoading = expenses?.status === "loading";
-
-  useEffect(() => {
-    if (scrollToForm && showCreateExpenseForm && formRef.current) {
-      formRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
-      setScrollToForm(false);
-    }
-  }, [scrollToForm, showCreateExpenseForm]);
 
   return (
     <>
@@ -182,24 +175,14 @@ export default function BudgetTab({
                   </div>
 
                   {/* Edit/Delete Buttons */}
-                  <div className="transform -translate-y-1/2 flex space-x-2 mt-4 flex justify-self-end">
-                    <button
-                      className="flex items-center px-2 py-1 rounded-md transition-all duration-200 bg-[#9B2C62]/10 text-[#9B2C62] hover:bg-[#9B2C62] hover:text-white text-xs"
-                      onClick={() => {
-                        setExpenseToEdit(expense);
-                        setShowCreateExpenseForm(true);
-                        setScrollToForm(true);
-                      }}
-                    >
-                      Edit expense
-                    </button>
-                    <button
-                      className="flex items-center px-2 py-1 rounded-md transition-all duration-200 bg-[#BE3455]/10 text-[#BE3455] hover:bg-[#BE3455] hover:text-white text-xs"
-                      onClick={() => handleExpenseDelete(expense._id)}
-                    >
-                      Delete expense
-                    </button>
-                  </div>
+                  <EditDeleteExpense
+                    showCreateExpenseForm={showCreateExpenseForm}
+                    setShowCreateExpenseForm={setShowCreateExpenseForm}
+                    handleExpenseDelete={handleExpenseDelete}
+                    setExpenseToEdit={setExpenseToEdit}
+                    formRef={formRef}
+                    expense={expense}
+                  />
                 </li>
               ))}
             </ul>
