7d044956928097dab5f7810a43fb51b471a36c4d
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-29 20:20:30 +0300

    loading dashboard func

--- src/components/taskManagerCollection/utils/eventsDashboardHelpers.jsx
+++ src/components/taskManagerCollection/utils/eventsDashboardHelpers.jsx
@@ -177,6 +177,13 @@ export const filterEvents = (
   });
 };
 
+export const LoadingDashboard = () => (
+  <div className="flex space-x-4 animate-pulse justify-center">
+    {[1, 2, 3, 4, 5].map((i) => (
+      <div key={i} className="bg-gray-100 rounded-lg p-4 w-80 h-64"></div>
+    ))}
+  </div>
+);
 
 
 
