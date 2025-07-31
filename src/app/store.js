a2dea43419df8436e063f584cecfb1b23d1076ba
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-31 19:44:49 +0300

    add clients reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import eventsReducer from "../redux/eventsSlice";
 import tasksReducer from "../redux/tasksSlice";
 import expensesReducer from "../redux/expensesSlice";
+import clientsReducer from "../redux/clientsSlice";
 
 const store = configureStore({
   reducer: {
     events: eventsReducer,
     tasks: tasksReducer,
     expenses: expensesReducer,
+    clients: clientsReducer,
   },
 });
 
