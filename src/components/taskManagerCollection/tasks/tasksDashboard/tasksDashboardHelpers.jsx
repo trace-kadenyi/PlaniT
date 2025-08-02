f4b24cd45fe461d4974d7794c238449e26802c68
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-03 00:33:20 +0300

    remove loading func

--- src/components/taskManagerCollection/tasks/tasksDashboard/tasksDashboardHelpers.jsx
+++ src/components/taskManagerCollection/tasks/tasksDashboard/tasksDashboardHelpers.jsx
@@ -193,17 +193,6 @@ export const filterTasks = (
   });
 };
 
-// loading state
-export const LoadingDashboard = () => {
-  return (
-    <div className="flex space-x-4 animate-pulse justify-center">
-      {[1, 2, 3, 4].map((i) => (
-        <div key={i} className="bg-gray-100 rounded-lg p-4 w-72 h-64"></div>
-      ))}
-    </div>
-  );
-};
-
 // update error render
 export const UpdateDashboardError = ({ updateError, dispatch }) => {
   return (
