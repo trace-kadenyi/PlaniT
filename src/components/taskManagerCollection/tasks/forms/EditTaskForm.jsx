e61cacb5155c291a93b764098a4f35e628ed33c8
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-04 13:07:25 +0300

    remove unnecessary import

--- src/components/taskManagerCollection/tasks/forms/EditTaskForm.jsx
+++ src/components/taskManagerCollection/tasks/forms/EditTaskForm.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 
 import { updateTask, resetTaskStatus } from "../../../../redux/tasksSlice";
 import { toastWithProgress } from "../../../../globalHooks/useToastWithProgress";
