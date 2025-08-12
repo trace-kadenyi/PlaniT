0c5dce26a2cb757822e559e0a2b44281e63b98ae
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-12 15:11:07 +0300

    remove padding

--- src/components/navigation/Layout.jsx
+++ src/components/navigation/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout() {
       <Sidebar />
       {/* Main Content */}
       <main className="flex-1 overflow-y-auto">
-        <div className="p-4 md:p-6">
+        <div>
           <Outlet />
         </div>
       </main>
