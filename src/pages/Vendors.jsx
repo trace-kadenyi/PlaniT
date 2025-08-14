48b0a27359c6d96ca9aff6110e58046598f41f05
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-14 14:04:25 +0300

    fix bug: responsiveness for medium screens

--- src/pages/Vendors.jsx
+++ src/pages/Vendors.jsx
@@ -48,15 +48,14 @@ export default function Vendors() {
   }, [dispatch]);
 
   return (
-    <div className="min-h-screen bg-white p-6">
+    <div className="min-h-screen bg-white p-3 sm:p-10">
       <div className="max-w-7xl mx-auto">
         {/* Header */}
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
-          <div>
-            <h1 className="text-3xl md:text-4xl font-bold text-[#9B2C62]">
-              Vendor Directory
-            </h1>
-          </div>
+        <div className="flex flex-col lg:flex-row justify-between items-start md:items-center mb-8 gap-4">
+          <h1 className="text-3xl md:text-4xl font-bold text-[#9B2C62] mt-12 text-center sm:text-start sm:mt-2">
+            Vendor Directory
+          </h1>
+
           <div className="flex flex-col sm:flex-row gap-4 w-full md:w-auto">
             <div className="relative flex-grow">
               <Search className="w-4 h-4 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
@@ -70,7 +69,7 @@ export default function Vendors() {
             </div>
             <button
               onClick={() => navigate("/vendors/new")}
-              className="bg-[#9B2C62] hover:bg-[#801f4f] text-white font-semibold px-4 py-2 rounded-lg flex items-center gap-2 justify-center"
+              className="bg-[#9B2C62] hover:bg-[#801f4f] text-white font-semibold px-5 py-2 rounded-lg font-medium transition-colors duration-200 flex items-center justify-center gap-2 whitespace-nowrap"
             >
               <Plus className="w-5 h-5" /> New Vendor
             </button>
