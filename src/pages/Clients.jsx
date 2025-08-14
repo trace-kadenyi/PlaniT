5dc3887d18594922b1c6d5adf054a5058796df77
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-14 14:04:37 +0300

    fix bug: responsiveness for medium and small screens

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -88,26 +88,23 @@ export default function Clients() {
     <main className="min-h-screen bg-[#FFF7ED] p-3 sm:p-10">
       <div className="max-w-7xl mx-auto">
         {/* Header Section */}
-        <div className="flex flex-col md:flex-row md:items-center md:justify-between mb-6 gap-4">
-          <div>
-            <h1 className="text-3xl md:text-4xl font-bold text-[#9B2C62] mt-10 text-center sm:text-start sm:mt-2">
-              Client Directory
-            </h1>
-            {/* <p className="text-[#F59E0B] mt-1">
+        <div className="flex flex-col lg:flex-row justify-between items-start md:items-center mb-8 gap-4">
+          <h1 className="text-3xl md:text-4xl font-bold text-[#9B2C62] mt-12 text-center sm:text-start sm:mt-2">
+            Client Directory
+          </h1>
+          {/* <p className="text-[#F59E0B] mt-1">
               {filteredClients.length}{" "}
               {filteredClients.length === 1 ? "client" : "clients"} found
             </p> */}
-          </div>
 
-          <div className="flex flex-col sm:flex-row gap-3">
-            <div className="relative flex-grow max-w-md">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <Search className="text-gray-400 w-4 h-4" />
-              </div>
+          <div className="flex flex-col sm:flex-row gap-4 w-full md:w-auto">
+            <div className="relative flex-grow">
+              <Search className="w-4 h-4 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+
               <input
                 type="text"
                 placeholder="Search clients..."
-                className="pl-10 pr-4 py-2 w-full rounded-lg border border-gray-300 focus:ring-2 focus:ring-[#9B2C62] focus:border-transparent"
+                className="pl-10 pr-4 py-2 border border-[#E3CBC1] rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-[#9B2C62]"
                 value={searchTerm}
                 onChange={(e) => {
                   setSearchTerm(e.target.value);
@@ -117,7 +114,7 @@ export default function Clients() {
             </div>
             <button
               onClick={() => navigate("/clients/new")}
-              className="bg-[#F59E0B] hover:bg-[#D97706] text-white px-5 py-2 rounded-lg font-medium transition-colors duration-200 flex items-center justify-center gap-2 whitespace-nowrap max-w-[150px]"
+              className="bg-[#F59E0B] hover:bg-[#D97706] text-white px-5 py-2 rounded-lg font-medium transition-colors duration-200 flex items-center justify-center gap-2 whitespace-nowrap"
             >
               <Plus className="w-5 h-5" /> New Client
             </button>
