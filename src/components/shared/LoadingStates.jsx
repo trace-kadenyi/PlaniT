62667fb1011d07a36d551658c60ff344df050590
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-24 14:11:54 +0300

    remove arrow functin from eventloading state

--- src/components/shared/LoadingStates.jsx
+++ src/components/shared/LoadingStates.jsx
@@ -1,14 +1,16 @@
 import { Loader2 } from "lucide-react";
 
 // event loading state
-export const EventLoadingState = () => (
-  <main className="min-h-screen flex items-center justify-center">
-    <div className="flex flex-col items-center space-y-2">
-      <div className="w-10 h-10 border-4 border-[#F59E0B]/30 border-t-[#F59E0B] rounded-full animate-spin"></div>
-      <p className="text-sm text-gray-500">Loading event details...</p>
-    </div>
-  </main>
-);
+export function EventLoadingState() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center space-y-2">
+        <div className="w-10 h-10 border-4 border-[#F59E0B]/30 border-t-[#F59E0B] rounded-full animate-spin"></div>
+        <p className="text-sm text-gray-500">Loading event details...</p>
+      </div>
+    </main>
+  );
+}
 
 // tasks loading state
 export const TasksLoadingState = () => (
