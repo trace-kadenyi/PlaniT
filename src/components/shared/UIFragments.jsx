6762a35968c7f89016cbf28f33949e2bf5f54076
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-24 14:23:17 +0300

    use regular function declaration for EventStatusPill

--- src/components/shared/UIFragments.jsx
+++ src/components/shared/UIFragments.jsx
@@ -1,20 +1,22 @@
 import { formatDateTime } from "../taskManagerCollection/utils/formatting";
 // handle event status colours
-export const EventStatusPill = ({ status }) => (
-  <span
-    className={`px-2 py-1 rounded text-xs ${
-      status === "Completed"
-        ? "bg-green-100 text-green-800"
-        : status === "Cancelled"
-        ? "bg-red-100 text-red-800"
-        : status === "In Progress"
-        ? "bg-[#F5EBFF] text-[#9B2C62]"
-        : "bg-[#EFF6FF] text-[#1E40AF]"
-    }`}
-  >
-    {status}
-  </span>
-);
+export function EventStatusPill({ status }) {
+  return (
+    <span
+      className={`px-2 py-1 rounded text-xs ${
+        status === "Completed"
+          ? "bg-green-100 text-green-800"
+          : status === "Cancelled"
+          ? "bg-red-100 text-red-800"
+          : status === "In Progress"
+          ? "bg-[#F5EBFF] text-[#9B2C62]"
+          : "bg-[#EFF6FF] text-[#1E40AF]"
+      }`}
+    >
+      {status}
+    </span>
+  );
+}
 
 // tasks status colours
 export const TaskStatusPill = ({ status }) => (
