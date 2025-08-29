c9171ee7ff0f8892c0a141d3ffbf2696f270f33a
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-29 21:31:20 +0300

    add dark theme to loading dashboard

--- src/components/taskManagerCollection/utils/genDashboardHelpers.jsx
+++ src/components/taskManagerCollection/utils/genDashboardHelpers.jsx
@@ -2,7 +2,10 @@
 export const LoadingDashboard = () => (
   <div className="flex space-x-4 animate-pulse justify-center">
     {[1, 2, 3, 4].map((i) => (
-      <div key={i} className="bg-gray-100 rounded-lg p-4 w-80 h-64"></div>
+      <div
+        key={i}
+        className="bg-gray-100 dark:bg-gray-700/60 rounded-lg p-4 w-80 h-64"
+      ></div>
     ))}
   </div>
 );
