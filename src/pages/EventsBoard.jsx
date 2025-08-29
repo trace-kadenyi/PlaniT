5f2f770673016ab9e94ae57bf919fd9ddc445ea0
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-29 14:38:10 +0300

    add dark bg

--- src/pages/EventsBoard.jsx
+++ src/pages/EventsBoard.jsx
@@ -123,12 +123,12 @@ export default function EventsBoard() {
   );
 
   return (
-    <div className="p-4 sm:px-10 sm:pt-10 pb-15 bg-white min-h-screen">
+    <div className="p-4 sm:px-10 sm:pt-10 pb-15 bg-white dark:bg-gradient-to-b dark:from-[#1a1026] dark:to-black min-h-screen">
       <div className="text-center mb-6">
-        <h1 className="text-2xl font-bold text-[#9B2C62] mt-10 mb-2 sm:my-2">
+        <h1 className="text-2xl font-bold text-[#9B2C62] dark:text-[#D97706] mt-10 mb-2 sm:my-2">
           Events Board
         </h1>
-        <p className="text-gray-600 max-w-4xl mx-auto mb-4">
+        <p className="text-gray-600 dark:text-gray-300 max-w-4xl mx-auto mb-4">
           Track and manage all your events in one place - drag and drop to
           update status!
         </p>
