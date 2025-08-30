23105bd380744f8ec1a25dfd095070f9f964305a
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-30 13:33:40 +0300

    adjust text for dark mode

--- src/pages/TasksBoard.jsx
+++ src/pages/TasksBoard.jsx
@@ -112,7 +112,7 @@ export default function TasksBoard() {
         <h1 className="text-2xl font-bold text-[#9B2C62] dark:text-[#D97706] mt-10 mb-2 sm:my-2">
           Tasks Board
         </h1>
-        <p className="text-gray-600 max-w-4xl mx-auto mb-4">
+        <p className="text-gray-600 dark:text-gray-300 max-w-4xl mx-auto mb-4">
           Drag and drop tasks between columns to update their status - stay
           organized and track your workflow at a glance!
         </p>
