e6cd4082b43c0195b7c511f6305319ac18d53b23
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-30 13:30:16 +0300

    modify handling of high priority tasks

--- src/components/shared/UIFragments.jsx
+++ src/components/shared/UIFragments.jsx
@@ -78,12 +78,16 @@ export function DatePill({ date, status }) {
 
 // handle tasks priorities
 export function TasksPriorityPill({ priority }) {
+  if (priority.toLowerCase() === "high") {
+    return null;
+  }
   return (
     <span
       className={`inline-block px-2 py-0.5 rounded-full font-medium ${
-        priority.toLowerCase() === "high"
-          ? "bg-[#F59E0B]/20 text-[#C2410C] dark:bg-red-900/40 dark:text-red-200"
-          : priority.toLowerCase() === "medium"
+        // priority.toLowerCase() === "high"
+        //   ?
+        //   // "bg-[#F59E0B]/20 text-[#C2410C] dark:bg-red-900/30 dark:text-red-200"
+        priority.toLowerCase() === "medium"
           ? "bg-yellow-100 text-yellow-700 dark:bg-yellow-900/40 dark:text-yellow-200"
           : "bg-gray-200 text-gray-600 dark:bg-gray-800/50 dark:text-gray-300"
       }`}
