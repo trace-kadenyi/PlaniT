f4aa09737cb9f6fd706110129d609a914997b5eb
Tracey Kadenyi <treykadenyi@gmail.com>
2025-08-31 16:31:02 +0300

    dark-mode progress bar

--- src/components/ui/ProgressBar.jsx
+++ src/components/ui/ProgressBar.jsx
@@ -1,12 +1,15 @@
 export default function ProgressBar({ value, className = "" }) {
-  const progressColor = value > 90 
-    ? "bg-[#9B2C62]" // Deep mulberry for critical
-    : value > 70 
+  const progressColor =
+    value > 90
+      ? "bg-[#9B2C62]" // Deep mulberry for critical
+      : value > 70
       ? "bg-[#D97706]" // Saffron gold for warning
       : "bg-[#F59E0B]"; // Lighter gold for normal
 
   return (
-    <div className={`w-full bg-[#F3EDE9] rounded-full h-2 ${className}`}>
+    <div
+      className={`w-full bg-[#F3EDE9] dark:bg-gray-400 rounded-full h-2 ${className}`}
+    >
       <div
         className={`h-2 rounded-full transition-all duration-300 ${progressColor}`}
         style={{ width: `${value}%` }}
