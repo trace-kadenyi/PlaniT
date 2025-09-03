f82d22241840ffd6ddfdca41d1a665b2eba4e138
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-04 01:37:24 +0300

    dark mode: edit and delete expense

--- src/components/shared/EditDeleteExpense.jsx
+++ src/components/shared/EditDeleteExpense.jsx
@@ -9,7 +9,7 @@ export default function EditDeleteExpense({
   return (
     <div className="transform -translate-y-1/2 flex space-x-2 mt-4 flex justify-self-end sm:min-w-[212px]">
       <button
-        className="flex items-center px-2 py-1 rounded-md transition-all duration-200 bg-[#9B2C62]/10 text-[#9B2C62] hover:bg-[#9B2C62] hover:text-white text-xs"
+        className="flex items-center px-2 py-1 rounded-md transition-all duration-200 bg-[#9B2C62]/10 text-[#9B2C62] hover:bg-[#9B2C62] hover:text-white text-xs dark:bg-[#F59E0B]/20 dark:text-gray-300 dark:hover:bg-[#F59E0B]/30"
         onClick={() => {
           setExpenseToEdit(expense);
           setShowCreateExpenseForm(true);
@@ -19,8 +19,10 @@ export default function EditDeleteExpense({
         Edit expense
       </button>
       <button
-        className="flex items-center px-2 py-1 rounded-md transition-all duration-200 bg-[#BE3455]/10 text-[#BE3455] hover:bg-[#BE3455] hover:text-white text-xs"
-        onClick={() => handleExpenseDelete(expense._id, expense.vendor?._id, expenses)}
+        className="flex items-center px-2 py-1 rounded-md transition-all duration-200 bg-[#BE3455]/10 text-[#BE3455] hover:bg-[#BE3455] hover:text-white text-xs dark:bg-[#BE3455]/30 dark:hover:bg-[#BE3455]/40"
+        onClick={() =>
+          handleExpenseDelete(expense._id, expense.vendor?._id, expenses)
+        }
       >
         Delete expense
       </button>
