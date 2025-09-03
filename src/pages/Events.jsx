6e5b34f6c02c6d204ef8c41df75fbfa2c31be49c
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-03 12:53:12 +0300

    fix bug: grid arrangement/responsiveness

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -204,7 +204,7 @@ export default function Events() {
                 {/* Events Grid */}
                 {expandedMonths[monthYear] && (
                   <div className="p-4 pt-2">
-                    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-4">
                       {monthEvents.map((event, index) => (
                         <EventCard
                           key={index}
