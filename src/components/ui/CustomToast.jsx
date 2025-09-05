3c88849e879adcbe7904650758c7177589454e86
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-05 16:01:29 +0300

    add dark mode

--- src/components/ui/CustomToast.jsx
+++ src/components/ui/CustomToast.jsx
@@ -14,8 +14,8 @@ export default function CustomToast({ message, t, duration = 4000 }) {
   }, [duration]);
 
   return (
-    <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-lg max-w-[300px]">
-      <p className="text-sm text-gray-800 mb-2">{message}</p>
+    <div className="p-4 bg-white dark:bg-gradient-to-b dark:from-[#1a1026] dark:to-black border border-gray-200 rounded-lg shadow-lg max-w-[300px]">
+      <p className="text-sm text-gray-800 dark:text-gray-300 mb-2">{message}</p>
       <div className="w-full h-1 bg-gray-200 rounded">
         <div
           className="h-1 bg-orange-400 transition-all duration-100"
