b2b697521ba9324d1d937662f397449f4b2abd06
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-05 16:09:05 +0300

    dark mode: border

--- src/components/ui/CustomToast.jsx
+++ src/components/ui/CustomToast.jsx
@@ -14,7 +14,7 @@ export default function CustomToast({ message, t, duration = 4000 }) {
   }, [duration]);
 
   return (
-    <div className="p-4 bg-white dark:bg-gradient-to-b dark:from-[#1a1026] dark:to-black border border-gray-200 rounded-lg shadow-lg max-w-[300px]">
+    <div className="p-4 bg-white dark:bg-gradient-to-b dark:from-[#1a1026] dark:to-black border border-gray-200 dark:border-gray-700 rounded-lg shadow-lg max-w-[300px]">
       <p className="text-sm text-gray-800 dark:text-gray-300 mb-2">{message}</p>
       <div className="w-full h-1 bg-gray-200 rounded">
         <div
