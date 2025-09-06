05dca88c99054b07456f0507104e882815f50c3f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-06 13:23:04 +0300

    fix bug: gen error state

--- src/components/shared/ErrorStates.jsx
+++ src/components/shared/ErrorStates.jsx
@@ -40,7 +40,7 @@ export function ErrorState({
 // gen error state
 export function GenErrorState({ error, message }) {
   return (
-    <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-6 rounded">
+    <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-6 rounded dark:bg-red-300">
       <p>
         {error} || {message}
       </p>
