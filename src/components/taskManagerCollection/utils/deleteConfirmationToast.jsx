92c8edb92eb6deb6f1e81460567d5dda8d243e8e
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-09 13:14:36 +0300

    add vendor

--- src/components/taskManagerCollection/utils/deleteConfirmationToast.jsx
+++ src/components/taskManagerCollection/utils/deleteConfirmationToast.jsx
@@ -27,6 +27,10 @@ export default function DeleteConfirmationToast({
       warning: "This will permanently delete the client.",
       confirm: "Yes, Delete Client",
     },
+    vendor: {
+      warning: "This will permanently delete the vendor.",
+      confirm: "Yes, Delete Vendor",
+    },
   };
 
   useEffect(() => {
