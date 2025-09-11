ca08ca44a96b0c4d71981e6e60673740228999dc
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-11 13:46:18 +0300

    link Add Budget directly to budget info on form

--- src/components/taskManagerCollection/tabs/BudgetTab.jsx
+++ src/components/taskManagerCollection/tabs/BudgetTab.jsx
@@ -58,7 +58,7 @@ export default function BudgetTab({
         </h2>
         {hasNoBudget ? (
           <Link
-            to={`/events/${id}/edit`}
+            to={`/events/${id}/edit/#budget_info_id`}
             className="flex items-center space-x-1 text-sm px-3 py-1.5 rounded-full bg-[#9B2C62] text-white hover:bg-[#7A2350] transition text-xs cursor-default dark:bg-[#D97706]/90 dark:text-white dark:hover:bg-[#D97706]/50"
           >
             <Plus className="w-3 h-3" />
