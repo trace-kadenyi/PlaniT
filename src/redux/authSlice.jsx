c5e5eb707a8c26837b1b4fc64aa47a8d7d93464f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-20 12:33:14 +0300

    loginuser thunk

---
+++ src/redux/authSlice.jsx
@@ -0,0 +1,24 @@
+// authSlice.jsx
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../app/api";
+
+// --- Async Thunks ---
+
+// Login user
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const res = await api.post("/api/auth/login", credentials);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err.response?.data || err.message);
+    }
+  }
+);
+
+
+
+
+
+
