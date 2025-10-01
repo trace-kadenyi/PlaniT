9ceea17681a8949400eee4f13b96718c2882ad94
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-01 14:25:50 +0300

    enhance protected route

--- src/components/user/ProtectedRoute.jsx
+++ src/components/user/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
