4cd4cf8331ae3b0e98e25aebf02fe01d9883cceb
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-01 14:01:42 +0300

    add authreducer

--- src/app/store.js
+++ src/app/store.js
@@ -4,10 +4,12 @@ import tasksReducer from "../redux/tasksSlice";
 import expensesReducer from "../redux/expensesSlice";
 import clientsReducer from "../redux/clientsSlice";
 import vendorsReducer from "../redux/vendorsSlice";
+import authReducer from "../redux/authSlice";
 import { setStore } from "./api";
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     events: eventsReducer,
     tasks: tasksReducer,
     expenses: expensesReducer,
