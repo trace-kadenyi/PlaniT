666d8d8ff0a8e2cbb9aeb999f4603445d30376be
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-06 15:00:10 +0300

    Show loading or wait if we're trying to refresh tokens

--- src/components/user/ProtectedRoute.jsx
+++ src/components/user/ProtectedRoute.jsx
@@ -1,8 +1,14 @@
+// components/ProtectedRoute.jsx
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated, loginStatus } = useSelector((state) => state.auth);
+
+  // Show loading or wait if we're trying to refresh tokens
+  if (loginStatus === "loading") {
+    return <div>Loading...</div>; // Or a proper loading component
+  }
 
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
