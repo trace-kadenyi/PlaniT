0c404bdda88af95d6f4554bb688dfbb18a72d43b
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-14 14:54:01 +0300

    only show delete all clients btn if there is a veritable client list

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -163,21 +163,24 @@ export default function Clients() {
               </button>
             ))}
           </div>
-          <div className="w-max mx-auto sm:mx-0">
-            <button
-              onClick={handleDeleteAll}
-              disabled={deleteAllStatus === "loading"}
-              className={`bg-[#9B2C62] hover:bg-[#801f4f] text-white px-4 py-2 rounded-lg font-medium text-sm transition-colors duration-200 flex items-center justify-center gap-2 whitespace-nowrap ${
-                deleteAllStatus === "loading"
-                  ? "opacity-50 cursor-not-allowed"
-                  : ""
-              }`}
-            >
-              {deleteAllStatus === "loading"
-                ? "Deleting..."
-                : "Delete All Clients"}
-            </button>
-          </div>
+          {/* delete all clients btn */}
+          {filteredClients.length > 0 && (
+            <div className="w-max mx-auto sm:mx-0">
+              <button
+                onClick={handleDeleteAll}
+                disabled={deleteAllStatus === "loading"}
+                className={`bg-[#9B2C62] hover:bg-[#801f4f] text-white px-4 py-2 rounded-lg font-medium text-sm transition-colors duration-200 flex items-center justify-center gap-2 whitespace-nowrap ${
+                  deleteAllStatus === "loading"
+                    ? "opacity-50 cursor-not-allowed"
+                    : ""
+                }`}
+              >
+                {deleteAllStatus === "loading"
+                  ? "Deleting..."
+                  : "Delete All Clients"}
+              </button>
+            </div>
+          )}
         </div>
 
         {/* Status Messages */}
