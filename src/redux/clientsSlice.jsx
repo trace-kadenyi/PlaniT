3b29c6092a02832c4c1a3cb37922d7984768c8e0
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-16 21:48:55 +0300

    modify comment

--- src/redux/clientsSlice.jsx
+++ src/redux/clientsSlice.jsx
@@ -64,7 +64,7 @@ export const deleteAllClients = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const res = await api.delete("/api/clients");
-      return res.data; // contains { message, deletedCount }
+      return res.data; // deletecount message
     } catch (err) {
       return rejectWithValue(
         err.response?.data || { message: "Failed to delete all clients" }
