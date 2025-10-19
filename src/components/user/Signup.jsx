9b0219945bfd609e40699d4d074206e42c20bf2e
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-19 14:54:48 +0300

    extract xicon

--- src/components/user/Signup.jsx
+++ src/components/user/Signup.jsx
@@ -9,7 +9,7 @@ import {
 } from "../../redux/authSlice";
 
 import LogoWordmark from "../navigation/LogoWordmark";
-import { CheckIcon } from "../ui/UserUiFragments";
+import { CheckIcon, XIcon } from "../ui/UserUiFragments";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -31,17 +31,6 @@ const Signup = () => {
     special: false,
   });
 
-  
-
-  const XIcon = () => (
-    <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
   const { signupStatus, signupError, isAuthenticated } = useSelector(
     (state) => state.auth
   );
