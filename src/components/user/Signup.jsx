3664d7f860e21402a5f6a61a5046de7e873ba531
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-19 14:53:57 +0300

    externalize checkicon

--- src/components/user/Signup.jsx
+++ src/components/user/Signup.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../redux/authSlice";
 
 import LogoWordmark from "../navigation/LogoWordmark";
+import { CheckIcon } from "../ui/UserUiFragments";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -30,16 +31,7 @@ const Signup = () => {
     special: false,
   });
 
-  // Add these icon components
-  const CheckIcon = () => (
-    <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  
 
   const XIcon = () => (
     <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
@@ -283,7 +275,6 @@ const Signup = () => {
                     </button>
                   </div>
                 </div>
-                {/* Password Requirements */}
                 {/* Password Requirements */}
                 <div className="space-y-1">
                   <p className="text-xs text-gray-500">
