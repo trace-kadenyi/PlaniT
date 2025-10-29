62addd4f5478dd12c25730906fcb6ad7d9322f95
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-29 14:35:38 +0300

    handle user functionality

---
+++ src/components/user/UserManagement.jsx
@@ -0,0 +1,71 @@
+// components/UserManagement.jsx
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchOrganizationUsers,
+  addOrganizationUser,
+  removeOrganizationUser,
+  updateUserRole,
+  resetOrganizationStatus,
+} from "../../redux/organizationSlice";
+
+const UserManagement = () => {
+  const dispatch = useDispatch();
+  const { users, status, addUserStatus, removeUserStatus, updateRoleStatus } = useSelector(
+    (state) => state.organization
+  );
+  const currentUser = useSelector((state) => state.auth.user);
+  
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    organizationRole: "planner",
+    password: "",
+  });
+
+  useEffect(() => {
+    dispatch(fetchOrganizationUsers());
+  }, [dispatch]);
+
+  // Reset status when component unmounts
+  useEffect(() => {
+    return () => {
+      dispatch(resetOrganizationStatus());
+    };
+  }, [dispatch]);
+
+  const handleAddUser = async (e) => {
+    e.preventDefault();
+    try {
+      await dispatch(addOrganizationUser(formData)).unwrap();
+      setShowAddForm(false);
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        organizationRole: "planner",
+        password: "",
+      });
+      // Refresh the user list
+      dispatch(fetchOrganizationUsers());
+    } catch (error) {
+      // Error is handled by the slice
+      console.error("Failed to add user:", error);
+    }
+  };
+
+ 
+  
+  
+ 
+
+  return (
+    <div className="max-w-6xl mx-auto p-6">
+     
+    </div>
+  );
+};
+
+export default UserManagement;
