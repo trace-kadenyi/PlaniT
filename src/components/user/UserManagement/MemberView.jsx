8bfdb2c2ce7a5945ccec33b41d208578e1103268
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-05 17:21:04 +0300

    add org name to member view

--- src/components/user/UserManagement/MemberView.jsx
+++ src/components/user/UserManagement/MemberView.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import UserList from "./UserList";
 
-const MemberView = ({ users, currentUser }) => (
+const MemberView = ({ users, currentUser, orgName }) => (
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
     <div className="mb-8">
       <div className="flex justify-between items-start">
         <div>
           <h1 className="text-3xl md:text-4xl font-bold text-[#9B2C62] dark:text-[#D97706] mt-12 text-center sm:text-start sm:mt-2">
-            Team Directory
+           {orgName} Team Directory
           </h1>
           <p className="text-gray-600 dark:text-gray-300 mt-2">
             View your team members and their roles within the organization
