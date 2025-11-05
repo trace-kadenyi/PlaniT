14ef0a8fc14bbd1f75b4594316884b712402d1fb
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-05 16:45:53 +0300

    fix overall page padding

--- src/components/user/UserManagement/MemberView.jsx
+++ src/components/user/UserManagement/MemberView.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import UserList from "./UserList";
 
 const MemberView = ({ users, currentUser }) => (
-  <div className="max-w-7xl mx-auto">
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
     <div className="mb-8">
       <div className="flex justify-between items-start">
         <div>
