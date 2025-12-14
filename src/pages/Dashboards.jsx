410b1285f891163734759dba581f338efc1311ad
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-14 16:05:33 +0300

    remove redundant code

--- src/pages/Dashboards.jsx
+++ src/pages/Dashboards.jsx
@@ -71,13 +71,6 @@ const Dashboards = () => {
   const eventsByStatus = groupEventByStatus(dashboardItems);
 
   // Group tasks by status
-  // const tasksByStatus = {
-  //   "To Do": tasks.filter((task) => task.status === "To Do"),
-  //   "In Progress": tasks.filter((task) => task.status === "In Progress"),
-  //   Completed: tasks.filter((task) => task.status === "Completed"),
-  //   "In Review": tasks.filter((task) => task.status === "In Review"),
-  // };
-
   const tasksByStatus = groupTasksByStatus(tasks);
 
   const dashboardCards = [
