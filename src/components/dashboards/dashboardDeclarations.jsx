25c92b2b44eb6bb60d1936f49e2bfc7332b40ef7
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-14 16:05:20 +0300

    function to group tasks by status

--- src/components/dashboards/dashboardDeclarations.jsx
+++ src/components/dashboards/dashboardDeclarations.jsx
@@ -80,3 +80,13 @@ export const groupEventByStatus = (dashboardItems) => {
     cancelled: dashboardItems.filter((event) => event.status === "Cancelled"),
   };
 };
+
+// Group tasks by status
+export const groupTasksByStatus = (tasks) => {
+  return {
+    "To Do": tasks.filter((task) => task.status === "To Do"),
+    "In Progress": tasks.filter((task) => task.status === "In Progress"),
+    Completed: tasks.filter((task) => task.status === "Completed"),
+    "In Review": tasks.filter((task) => task.status === "In Review"),
+  };
+};
