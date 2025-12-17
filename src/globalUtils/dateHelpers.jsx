d99b639c4e66c447aafac49b904db23c404dae78
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-17 20:14:20 +0300

    getDateBadgeClass func

--- src/globalUtils/dateHelpers.jsx
+++ src/globalUtils/dateHelpers.jsx
@@ -54,3 +54,23 @@ export const formatDashDate = (dateString) => {
     day: "numeric",
   });
 };
+
+// get date badge class
+export const getDateBadgeClass = (date) => {
+  const formattedDate = formatDashDate(date);
+
+  const isUrgent =
+    formattedDate.includes("day") ||
+    formattedDate.includes("Tomorrow") ||
+    formattedDate.includes("Today") ||
+    formattedDate.includes("Overdue");
+
+  const baseClasses = "text-xs px-2 py-1 rounded-full whitespace-nowrap ml-2";
+
+  const urgentClasses =
+    "text-red-600 font-semibold bg-red-200 dark:bg-red-400/10";
+
+  const normalClasses = "text-[#9B2C62] dark:text-[#F59E0B] bg-[#F59E0B]/10";
+
+  return `${baseClasses} ${isUrgent ? urgentClasses : normalClasses}`;
+};
