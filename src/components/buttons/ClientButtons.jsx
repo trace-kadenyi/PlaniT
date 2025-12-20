494a9adac56c78fd2fc890bec349a6da313e1b36
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-20 17:48:02 +0300

    modify import path

--- src/components/buttons/ClientButtons.jsx
+++ src/components/buttons/ClientButtons.jsx
@@ -1,6 +1,6 @@
 import { Plus, Archive, RefreshCcw, Trash2, Edit3 } from "lucide-react";
 
-import PermissionButton from "../ui/PermissionButton";
+import PermissionButton from "./PermissionButton";
 import { PERMISSIONS, RESOURCES } from "../../globalHooks/userPermissions";
 
 // create new client
