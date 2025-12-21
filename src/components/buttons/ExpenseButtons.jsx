2ee8f277ef3d3f6fe31ab3ee099d16a44b28c8a8
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-21 17:35:43 +0300

    AddExpenseBtn func

--- src/components/buttons/ExpenseButtons.jsx
+++ src/components/buttons/ExpenseButtons.jsx
@@ -1,5 +1,40 @@
+import { XCircle, Plus } from "lucide-react";
+
 import PermissionButton from "./PermissionButton";
 import { PERMISSIONS, RESOURCES } from "../../globalHooks/userPermissions";
+
+// add expense btn
+export const AddExpenseBtn = ({
+  showCreateExpenseForm,
+  setExpenseToEdit,
+  setScrollToForm,
+  setShowCreateExpenseForm,
+}) => {
+  return (
+    <PermissionButton
+      permission={PERMISSIONS.CREATE}
+      resource={RESOURCES.EXPENSE}
+      tooltipTitle="Create a new expense"
+      fallbackTooltip="Upgrade to Planner or Admin role to create expenses"
+      onClick={() => {
+        if (showCreateExpenseForm) {
+          setExpenseToEdit(null);
+        } else {
+          setScrollToForm(true);
+        }
+        setShowCreateExpenseForm(!showCreateExpenseForm);
+      }}
+      className="flex items-center space-x-1 text-sm px-3 py-1.5 rounded-full bg-[#BE3455]/10 text-[#BE3455] hover:bg-[#BE3455]/20 transition text-xs cursor-pointer dark:bg-[#D97706]/90 dark:text-white dark:hover:bg-[#D97706]/50"
+    >
+      {showCreateExpenseForm ? (
+        <XCircle className="w-3 h-3" />
+      ) : (
+        <Plus className="w-3 h-3" />
+      )}
+      <span>{showCreateExpenseForm ? "Cancel" : "Add Expense"}</span>
+    </PermissionButton>
+  );
+};
 
 // add expense form field
 export const AddExpenseFormBtn = ({ onClose, expenseStatus, mode }) => {
