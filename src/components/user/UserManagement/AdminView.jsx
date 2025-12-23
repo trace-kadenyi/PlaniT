bf4a3d388a36b5e6310e8492caedfb53247792f6
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-23 16:13:52 +0300

    use AddNewMembersBtn

--- src/components/user/UserManagement/AdminView.jsx
+++ src/components/user/UserManagement/AdminView.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import UserList from "./UserList";
 import { Plus } from "lucide-react";
+import { AddNewMembersBtn } from "../../buttons/UserButtons";
 
 const AdminView = ({
   users,
@@ -22,13 +23,7 @@ const AdminView = ({
             Manage your organization members and their permissions
           </p>
         </div>
-        <button
-          onClick={onAddUser}
-          className="bg-[#9B2C62] hover:bg-[#801f4f] text-white font-semibold px-5 py-2 rounded-lg font-medium transition-colors duration-200 flex items-center justify-center gap-2 whitespace-nowrap"
-        >
-          <Plus className="w-5 h-5" />
-          Add Team Member
-        </button>
+        <AddNewMembersBtn onAddUser={onAddUser} />
       </div>
 
       <div className="mt-6 p-4 bg-white rounded-2xl shadow-lg border border-gray-100  dark:bg-gradient-to-br dark:from-gray-900 dark:to-black dark:border-r dark:border-gray-900/10 dark:hover:shadow-[0_4px_15px_rgba(255,255,255,0.05)]">
