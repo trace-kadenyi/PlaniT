82170d7d75cdc28c97d5b72644f8b77de6bcfce1
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-28 17:09:46 +0300

    fix bug: link to user and not currentuser

--- src/components/user/UserManagement/UserList.jsx
+++ src/components/user/UserManagement/UserList.jsx
@@ -49,10 +49,7 @@ const UserListItem = ({ user, editable, onRoleChange, onRemoveUser }) => {
 
   return (
     <div className="px-6 py-4 flex flex-col gap-3 sm:items-center justify-between sm:flex-row">
-      <a
-        href={`users/${currentUser._id}`}
-        className="flex items-center space-x-4"
-      >
+      <a href={`users/${user._id}`} className="flex items-center space-x-4">
         <div className="w-10 h-10 bg-[#9B2C62] rounded-full flex items-center justify-center">
           <span className="text-white font-semibold text-sm">
             {user.firstName[0]}
