fa2b694a9e1c2ab98adf78d9dbd15af3e4e1a351
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-28 12:38:27 +0300

    remove unused code

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,3 @@
-// pages/UserProfile.jsx
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
