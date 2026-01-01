3f0212d7ed2a309e25f3afd6e27f09bb35b12b1a
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-01 13:51:27 +0300

    formatYearMonthDay func

--- src/globalUtils/dateHelpers.jsx
+++ src/globalUtils/dateHelpers.jsx
@@ -74,3 +74,15 @@ export const getDateBadgeClass = (date) => {
 
   return `${baseClasses} ${isUrgent ? urgentClasses : normalClasses}`;
 };
+
+// year|month|day
+
+export const formatYearMonthDay = (dateString) => {
+  if (!dateString) return "Never";
+  return new Date(dateString).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
