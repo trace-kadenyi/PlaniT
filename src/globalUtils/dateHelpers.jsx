5ece8f8a134b4f9c88306800601cd3519f9a37a7
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-01 13:51:37 +0300

    formatHourMinute func

--- src/globalUtils/dateHelpers.jsx
+++ src/globalUtils/dateHelpers.jsx
@@ -86,3 +86,12 @@ export const formatYearMonthDay = (dateString) => {
   });
 };
 
+// hour|minute
+export const formatHourMinute = (dateString) => {
+  if (!dateString) return "Never logged in";
+  const date = new Date(dateString);
+  return `${date.toLocaleDateString()} at ${date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  })}`;
+};
