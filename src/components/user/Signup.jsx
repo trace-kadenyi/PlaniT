fd2975829f4e8a4ab79819a3883782d8b6168c88
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-02 19:41:39 +0300

    undo earlier change

--- src/components/user/Signup.jsx
+++ src/components/user/Signup.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import { signupUser, clearAuthErrors } from "../../redux/authSlice";
+import {
+  signupUser,
+  resetSignupState,
+  clearAuthErrors,
+} from "../../redux/authSlice";
 
 import LogoWordmark from "../navigation/LogoWordmark";
 import Password from "../shared/Password";
