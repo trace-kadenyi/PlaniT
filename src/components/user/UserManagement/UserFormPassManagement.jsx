49017633977067ef11bbde97d851843584d4e059
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-06 19:40:48 +0300

    remove unused imports

--- src/components/user/UserManagement/UserFormPassManagement.jsx
+++ src/components/user/UserManagement/UserFormPassManagement.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Key, Eye, EyeOff } from "lucide-react";
+import { Key } from "lucide-react";
 
 import Password, { generateRandomPassword } from "../../shared/Password";
 import { PasswordInput } from "../../ui/Button";
