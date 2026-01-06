2e579a5f41141cef421b8bd5c0a055247818f85f
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-06 13:06:28 +0300

    extract btn trigger to show pass fields

--- src/components/user/forms/EditUserForm.jsx
+++ src/components/user/forms/EditUserForm.jsx
@@ -9,7 +9,9 @@ import {
 } from "../../../globalHooks/usePermissionHelpers";
 import { EditUserFormBtn } from "../../buttons/UserButtons";
 import Password, { generateRandomPassword } from "../../shared/Password";
-import UserFormPassword from "../UserManagement/UserFormPassword";
+import UserFormPassword, {
+  ShowPassFieldsBtn,
+} from "../UserManagement/UserFormPassword";
 
 export function EditUserForm({
   handleSubmit,
@@ -164,30 +166,10 @@ export function EditUserForm({
 
       {/* button to show password fields */}
       {showPasswordFields && !showPasswordChange && (
-        <div className="bg-white dark:bg-gradient-to-br dark:from-gray-900 dark:to-black rounded-2xl shadow-lg border border-[#E3CBC1] dark:border-gray-800 p-6 mb-6">
-          <div className="flex justify-between items-center flex-wrap gap-2">
-            <h2 className="text-xl font-bold text-gray-900 dark:text-white flex items-center gap-2">
-              <Key className="w-5 h-5 text-[#9B2C62] dark:text-[#D97706]" />
-              Password
-            </h2>
-            <button
-              type="button"
-              onClick={() => setShowPasswordChange(true)}
-              className="text-sm text-[#9B2C62] dark:text-[#D97706] hover:underline font-medium"
-            >
-              Change Password
-            </button>
-          </div>
-          <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm">
-            Your password can be changed here. Click "Change Password" to
-            update.
-          </p>
-          {isSelf && (
-            <p className="text-gray-500 dark:text-gray-400 mt-2 text-xs font-semibold">
-              You will have to log in again after password change.
-            </p>
-          )}
-        </div>
+        <ShowPassFieldsBtn
+          setShowPasswordChange={setShowPasswordChange}
+          isSelf={isSelf}
+        />
       )}
       {/* Role Section */}
       <div className="bg-white dark:bg-gradient-to-br dark:from-gray-900 dark:to-black rounded-2xl shadow-lg border border-[#E3CBC1] dark:border-gray-800 p-6 mb-8">
