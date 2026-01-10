4cd84a0de38142d067359f7b653cd87ecfac974d
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-10 15:44:27 +0300

    use CirclePlus

--- src/components/taskManagerCollection/expenses/ExpenseAuditLogPanel.jsx
+++ src/components/taskManagerCollection/expenses/ExpenseAuditLogPanel.jsx
@@ -484,20 +484,7 @@ const ExpenseAuditLogPanel = ({ eventId }) => {
 
                               {/* Created on */}
                               <div className="flex items-center gap-2">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  className="w-3 h-3 text-gray-500 dark:text-gray-400 flex-shrink-0"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  stroke="currentColor"
-                                  strokeWidth={2}
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                                  />
-                                </svg>
+                                <CirclePlus className="w-3 h-3 text-gray-500 dark:text-gray-400 flex-shrink-0" />
                                 <div>
                                   <span className="text-gray-600 dark:text-gray-400 mr-1">
                                     Created on:
