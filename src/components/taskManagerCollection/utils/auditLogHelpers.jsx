e135eefa80d77c67005aae0b9e6184ff9d9e4fe5
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-11 16:47:10 +0300

    getActionColor func

--- src/components/taskManagerCollection/utils/auditLogHelpers.jsx
+++ src/components/taskManagerCollection/utils/auditLogHelpers.jsx
@@ -46,4 +46,20 @@ export const getActionIcon = (actionType) => {
   }
 };
 
+export const getActionColor = (actionType) => {
+  switch (actionType) {
+    case "CREATE":
+      return "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200";
+    case "UPDATE":
+      return "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200";
+    case "DELETE":
+      return "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200";
+    case "AMOUNT_CHANGE":
+      return "bg-amber-100 text-amber-800 dark:bg-amber-900 dark:text-amber-200";
+    case "STATUS_CHANGE":
+      return "bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-200";
+    default:
+      return "bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200";
+  }
+};
 
