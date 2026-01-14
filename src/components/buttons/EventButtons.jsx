5e295cc43e268c033b1c5f07e83fb789c24e22c3
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-14 16:01:06 +0300

    Handle delete properly

--- src/components/buttons/EventButtons.jsx
+++ src/components/buttons/EventButtons.jsx
@@ -36,7 +36,12 @@ export const NoEventBtn = ({ navigate }) => {
 };
 
 // edit/delete event btns
-export const EditDeleteEventBtns = ({ navigate, eventID, handleDelete }) => {
+export const EditDeleteEventBtns = ({
+  navigate,
+  eventID,
+  eventName,
+  handleDelete,
+}) => {
   return (
     <div className="px-5 pb-3 flex space-x-2 lg:opacity-0 lg:group-hover:opacity-100 transition">
       {/* edit event */}
@@ -59,7 +64,13 @@ export const EditDeleteEventBtns = ({ navigate, eventID, handleDelete }) => {
         resource={RESOURCES.EVENT}
         tooltipTitle="Delete event"
         fallbackTooltip="Upgrade to Planner or Admin role to delete events"
-        onClick={() => handleDelete(eventID)}
+        onClick={() =>
+          handleDelete(eventID, {
+            type: "event",
+            entityName: eventName, // optional
+            onSuccess: () => navigate("/events"),
+          })
+        }
         className="flex items-center space-x-1 text-sm px-2 py-1 rounded-full bg-red-100/30 text-red-600 hover:bg-red-200 transition text-xs cursor-pointer dark:bg-red-900/30 dark:text-red-300 dark:hover:bg-red-900/50"
         // title="Delete"
       >
