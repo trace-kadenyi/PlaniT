f475a4e4b1e4b14ccf2cb54c28640493312f4341
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-14 14:03:11 +0300

    return dispatch

--- src/components/taskManagerCollection/utils/handlers/taskHandlers.jsx
+++ src/components/taskManagerCollection/utils/handlers/taskHandlers.jsx
@@ -66,7 +66,7 @@ export const createTaskDeleteHandler = (
             duration={duration}
             type="task"
             onConfirm={() => {
-              dispatch(deleteTask(taskId))
+              return dispatch(deleteTask(taskId))
                 .unwrap()
                 .then(() => {
                   toastWithProgress("Task deleted successfully");
