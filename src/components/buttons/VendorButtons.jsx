cde2963003e809dce35fcb9370002c800f11631f
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-14 20:41:52 +0300

    disable cancel button on saving

--- src/components/buttons/VendorButtons.jsx
+++ src/components/buttons/VendorButtons.jsx
@@ -228,7 +228,8 @@ export const SaveVendorFormBtn = ({ formStatus, onCancel }) => {
         <button
           type="button"
           onClick={onCancel}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-6 py-2 rounded-lg dark:bg-gray-500 dark:hover:bg-gray-400"
+          disabled={formStatus === "loading"}
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-6 py-2 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
         >
           Cancel
         </button>
