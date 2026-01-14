e884dc025b8b85dd612f49ca87dda9e4c116feb3
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-14 22:15:09 +0300

    add navigate prop

--- src/pages/EditUserProfile.jsx
+++ src/pages/EditUserProfile.jsx
@@ -252,6 +252,7 @@ export default function EditUserProfile() {
           watch={watch}
           setValue={setValue}
           isEditConfirmActive={isEditConfirmActive}
+          navigate={navigate}
         />
       </div>
     </main>
