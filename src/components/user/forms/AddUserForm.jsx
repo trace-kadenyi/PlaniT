736b87579a97eac5765eeadebba6599f4a161a80
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-15 20:16:53 +0300

    add comment

--- src/components/user/forms/AddUserForm.jsx
+++ src/components/user/forms/AddUserForm.jsx
@@ -187,6 +187,7 @@ const AddUserForm = ({
                   )}
                 </div>
                 <div className="space-y-1">
+                  {/* last name */}
                   <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
                     Last Name *
                   </label>
