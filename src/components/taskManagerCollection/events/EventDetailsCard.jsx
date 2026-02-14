856c17fa37943a604333f9d84df00f26b9001aaa
Tracey Kadenyi <treykadenyi@gmail.com>
2026-02-14 14:18:33 +0300

    remove deletepaidtotal prop

--- src/components/taskManagerCollection/events/EventDetailsCard.jsx
+++ src/components/taskManagerCollection/events/EventDetailsCard.jsx
@@ -97,7 +97,6 @@ export default function EventDetailsCard({
       {expensesState.budgetStatus && (
         <BudgetOverview
           budgetStatus={expensesState.budgetStatus}
-          deletedPaidTotal={expensesState.deletedPaidTotal}
           eventID={id}
           notes={event?.budget?.notes}
           isArchived={event?.isArchived}
