c8f65c98eefa360c12e80a1c254a596fd300b258
Tracey Kadenyi <treykadenyi@gmail.com>
2026-03-09 15:31:45 +0300

    filter out deleted events from dashboard

--- src/redux/eventsSlice.jsx
+++ src/redux/eventsSlice.jsx
@@ -484,7 +484,7 @@ const eventsSlice = createSlice({
       .addCase(fetchEventsForDashboard.fulfilled, (state, action) => {
         state.dashboardStatus = "succeeded";
         state.dashboardItems = action.payload.filter(
-          (event) => !event.isArchived,
+          (event) => !event.isArchived && !event.isDeleted,
         );
       })
       .addCase(fetchEventsForDashboard.rejected, (state, action) => {
